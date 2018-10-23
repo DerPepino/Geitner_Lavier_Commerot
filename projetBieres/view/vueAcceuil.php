68e0be45fa039eb5583b56a0b1f5a4b26e108fab
GEITNER Teva <geitnert@hostname.iut2.upmf-grenoble.fr>
2018-10-23 08:48:50 +0200

    f

--- projetBieres/view/vueAcceuil.php
+++ projetBieres/view/vueAcceuil.php
@@ -2,12 +2,13 @@
 <head>
   <meta charset="utf-8">
   <title>BièresAGogo.com</title>
-  <link rel="stylesheet" href="vueAcceuil.css">
+  <link rel="stylesheet" media="screen" type="text/css" title="Exemple" href="vueAcceuil.css"/>
+
 </head>
 <body>
 
   <ul>
-    <li><a href="#5">Nouveautés</a></li>
+    <li><a href="#0">Nouveautés</a></li>
     <li><a href="#1">Catégories</a></li>
     <li><a href="#2">Mon panier</a></li>
     <li><a href="#3">Se connecter</a></li>
