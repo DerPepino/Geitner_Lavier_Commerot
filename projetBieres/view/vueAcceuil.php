c5ec77cacd3f2d61ed5db3d742f68607154de474
GEITNER Teva <geitnert@hostname.iut2.upmf-grenoble.fr>
2018-10-23 08:35:40 +0200

    tyj

--- projetBieres/view/vueAcceuil.php
+++ projetBieres/view/vueAcceuil.php
@@ -5,8 +5,14 @@
   <link rel="stylesheet" href="vueAcceuil.css">
 </head>
 <body>
-  ...
-  <!-- Le reste du contenu -->
-  ...
+
+  <ul>
+    <li><a href="#5">Nouveautés</a></li>
+    <li><a href="#1">Catégories</a></li>
+    <li><a href="#2">Mon panier</a></li>
+    <li><a href="#3">Se connecter</a></li>
+    <li><a href="#4">S'inscrire</a></li>
+  </ul>
+
 </body>
 </html>
