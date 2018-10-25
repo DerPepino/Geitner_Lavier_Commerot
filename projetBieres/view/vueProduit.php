a755e5102f8c80bd07d987fb92fbe119a90e16ce
LAVIER Pierre <lavierp@hostname.iut2.upmf-grenoble.fr>
2018-10-25 09:55:13 +0200

    zg

---
+++ projetBieres/view/vueProduit.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>BièresAGogo.com</title>
+  <link rel="stylesheet" href="vueProduit.css"/>
+
+</head>
+<body>
+  <div class="menu">
+   <img src="logoBeerHose.jpg">
+   <h1>BièresAGogo.com</h1>
+    <ul>
+      <li><a href="#0">Nouveautés</a></li>
+      <li><a href="#1">Catégories</a></li>
+      <li><a href="#2">Mon panier</a></li>
+      <li><a href="#3">Se connecter</a></li>
+      <li><a href="#4">S'inscrire</a></li>
+    </ul>
+  </div>
+  <div class="produit">
+    <image src="ImageProduit.jpeg">
+  </div>
+</body>
+</html>
